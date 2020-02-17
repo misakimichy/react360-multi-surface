11ab4b4f1438501fef6324f0ee6098c81e140658
misakimichy <misaki.koonce@gmail.com>
2020-02-17 15:08:54 -0800

    Initialize the multi-surface and 3D

---
+++ CurrentPost.js
@@ -0,0 +1,3 @@
+import * as React from 'react'
+import { StyleSheet, Text, View, VirButton } from 'react-360'
+import { connect } from './Store'
---
+++ client.js
@@ -0,0 +1,43 @@
+// This file contains the boilerplate to execute your React app.
+// If you want to modify your application's content, start in "index.js"
+
+import { ReactInstance, Location, Surface } from 'react-360-web';
+
+function init(bundle, parent, options = {}) {
+  const r360 = new ReactInstance(bundle, parent, {
+    // Add custom options here
+    fullScreen: true,
+    ...options,
+  });
+
+  // Create three roots: two flat panels on the left and the right, and a Location
+  // to mount rendered models in 3D space
+  const leftPanel = new Surface(300, 600, Surface.SurfaceShape.Flat)
+  leftPanel.setAngle(-0.6, 0)
+
+  const rightPanel = new Surface(300, 600, Surface.SurfaceShape.Flat)
+  rightPanel.setAngle(0.6, 0)
+  r360.renderToSurface(
+    r360.createRoot('TopPosts'),
+    leftPanel
+  )
+  r360.renderToSurface(
+    r360.createRoot('CurrentPost'),
+    rightPanel
+  )
+  r360.renderToLocation(
+    r360.createRoot('ModelView'),
+    new Location([0, -2, -10]),
+  )
+
+  // Render your app content to the default cylinder surface
+  // r360.renderToSurface(
+  //   r360.createRoot('react360_project', { /* initial props */ }),
+  //   r360.getDefaultSurface()
+  // );
+
+  // Load the initial environment
+  r360.compositor.setBackground(r360.getAssetURL('cat1.jpeg'));
+}
+
+window.React360 = {init};
